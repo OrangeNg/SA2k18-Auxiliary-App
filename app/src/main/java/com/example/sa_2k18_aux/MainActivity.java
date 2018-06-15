370585ce999a60b7c0640c5ab5b9733f5e67ee14
OrangeNg <ngquanhao@hotmail.com>
2018-06-15 23:16:49 +0800

    Firebase usage testing.

--- app/src/main/java/com/example/sa_2k18_aux/MainActivity.java
+++ app/src/main/java/com/example/sa_2k18_aux/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.sa_2k18_aux;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference dbRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onAddFoodClicked(View view){
+        //String[] foods = {"Chicken Rice", "Gardening Shovel"};
+        //dbRef.child("food").setValue(foods);
+    }
+
+
 }
