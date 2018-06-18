97ba99c1facbe420dafa750ab1580ac2ca6793b4
OrangeNg <ngquanhao@hotmail.com>
2018-06-18 19:32:02 +0800

    Improved validation for health vitals input. Added timestamp tracking for health vitals logging.

--- app/src/main/java/com/example/sa_2k18_aux/InputHealthActivity.java
+++ app/src/main/java/com/example/sa_2k18_aux/InputHealthActivity.java
@@ -10,6 +10,10 @@ import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class InputHealthActivity extends AppCompatActivity {
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -17,7 +21,10 @@ public class InputHealthActivity extends AppCompatActivity {
 
     private EditText bodyTempET;
     private EditText pulseRateET;
-    private EditText bloodPressureET;
+    private EditText systolicBloodPressureET;
+    private EditText diastolicBloodPressureET;
+
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,8 @@ public class InputHealthActivity extends AppCompatActivity {
 
         bodyTempET = findViewById(R.id.body_temp);
         pulseRateET = findViewById(R.id.pulse_rate);
-        bloodPressureET = findViewById(R.id.blood_pressure);
+        systolicBloodPressureET = findViewById(R.id.systolic_blood_pressure);
+        diastolicBloodPressureET =findViewById(R.id.diastolic_blood_pressure);
     }
 
     public void onBackClicked(View view){
@@ -37,27 +45,46 @@ public class InputHealthActivity extends AppCompatActivity {
     public void onInputClicked(View view){
 
         String bodyTemp = bodyTempET.getText().toString().trim();
-        bodyTempET.setText("");
         String pulseRate = pulseRateET.getText().toString().trim();
-        pulseRateET.setText("");
-        String bloodPressure = bloodPressureET.getText().toString().trim();
-        bloodPressureET.setText("");
+        String systolicBloodPressure = systolicBloodPressureET.getText().toString().trim();
+        String diastolicBloodPressure = diastolicBloodPressureET.getText().toString().trim();
 
-        if(checkValidity(bodyTemp, pulseRate, bloodPressure)){
+        if(checkValidity(bodyTemp, pulseRate, systolicBloodPressure, diastolicBloodPressure)){
             float bodyTempFloat = Float.parseFloat(bodyTemp);
             int pulseRateInt = Integer.parseInt(pulseRate);
-            float bloodPressureFloat = Float.parseFloat(bloodPressure);
+            float systolicBloodPressureFloat = Float.parseFloat(systolicBloodPressure);
+            float diastolicBloodPressureFloat = Float.parseFloat(diastolicBloodPressure);
 
             DatabaseReference healthInfoRef = dbRef.child("health_info");
-            healthInfoRef.child("body_temp").setValue(bodyTempFloat);
-            healthInfoRef.child("pulse_rate").setValue(pulseRateInt);
-            healthInfoRef.child("blood_pressure").setValue(bloodPressureFloat);
+            DatabaseReference infoRef = healthInfoRef.push();
+            infoRef.child("body_temp").setValue(bodyTempFloat);
+            infoRef.child("pulse_rate").setValue(pulseRateInt);
+            infoRef.child("sys_blood_pressure").setValue(systolicBloodPressureFloat);
+            infoRef.child("dias_blood_pressure").setValue(diastolicBloodPressureFloat);
+
+            Date currentTime = Calendar.getInstance().getTime();
+            String timeStamp = sdf.format(currentTime);
+            infoRef.child("timestamp").setValue(timeStamp);
+
+
+            bodyTempET.setText("");
+            pulseRateET.setText("");
+            systolicBloodPressureET.setText("");
+            diastolicBloodPressureET.setText("");
         }
     }
 
-    private boolean checkValidity(String bodyTemp, String pulseRate, String bloodPressure){
+    private boolean checkValidity(String bodyTemp, String pulseRate, String systolicBloodPressure, String diastolicBloodPressure){
         try {
             float bodyTempFloat = Float.parseFloat(bodyTemp);
+            if(bodyTempFloat < 35.0 || bodyTempFloat > 41.0){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Please enter a valid body temperature!")
+                        .setTitle("Temperature invalid");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
         }
         catch(NumberFormatException e){
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -70,6 +97,14 @@ public class InputHealthActivity extends AppCompatActivity {
 
         try {
             int pulseRateFloat = Integer.parseInt(pulseRate);
+            if(pulseRateFloat < 0 || pulseRateFloat > 220){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Please enter a valid pulse rate!")
+                        .setTitle("Pulse rate invalid");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
         }
         catch(NumberFormatException e){
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -81,12 +116,53 @@ public class InputHealthActivity extends AppCompatActivity {
         }
 
         try {
-            float bloodPressureFloat = Float.parseFloat(bloodPressure);
+            float systolicBloodPressureFloat = Float.parseFloat(systolicBloodPressure);
+            if(systolicBloodPressureFloat < 60 || systolicBloodPressureFloat > 200){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Please enter a valid systolic blood pressure!")
+                        .setTitle("Blood Pressure invalid");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+
         }
         catch(NumberFormatException e){
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("Please enter a valid blood pressure!")
+            builder.setMessage("Please enter a valid systolic blood pressure!")
                     .setTitle("Number format error");
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return false;
+        }
+
+        try {
+            float diastolicBloodPressureFloat = Float.parseFloat(diastolicBloodPressure);
+            if(diastolicBloodPressureFloat < 40 || diastolicBloodPressureFloat > 180){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Please enter a valid diastolic blood pressure!")
+                        .setTitle("Blood Pressure invalid");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+
+        }
+        catch(NumberFormatException e){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Please enter a valid diastolic blood pressure!")
+                    .setTitle("Number format error");
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            return false;
+        }
+
+        float sysBloodPressure = Float.parseFloat(systolicBloodPressure);
+        float diasBloodPressure = Float.parseFloat(diastolicBloodPressure);
+        if (sysBloodPressure <= diasBloodPressure){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Systolic blood pressure has to be higher than diastolic blood pressure!")
+                    .setTitle("Blood pressures invalid");
             AlertDialog dialog = builder.create();
             dialog.show();
             return false;
