5bcf4b4075be8b0a3ff8e32295c260eafdc25608
OrangeNg <ngquanhao@hotmail.com>
2018-06-19 15:19:11 +0800

    Improved food removal. Food is now removed from list by tapping food item. (Previously, users had to manually type in food name)

--- app/src/main/java/com/example/sa_2k18_aux/RemoveFoodActivity.java
+++ app/src/main/java/com/example/sa_2k18_aux/RemoveFoodActivity.java
@@ -1,11 +1,13 @@
 package com.example.sa_2k18_aux;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,51 +15,69 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class RemoveFoodActivity extends AppCompatActivity {
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference dbRef = database.getReference();
-    private EditText removeFoodET;
+
+    private ListView foodLV;
+    private ArrayAdapter<String> foodAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove_food);
+        foodLV = findViewById(R.id.food_list);
 
-        removeFoodET = findViewById(R.id.remove_food);
+        dbRef.child("food").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                populateList(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //Do nothing
+            }
+        });
+
     }
 
-    public void onRemoveClicked(View view){
-        final String food = removeFoodET.getText().toString().trim();
-
-        if(food.equals("")){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("No food written!")
-                    .setTitle("Empty field");
-            AlertDialog dialog = builder.create();
-            dialog.show();
+    private void populateList(DataSnapshot dataSnapshot){
+        ArrayList<String> foods = new ArrayList<>();
+        for(DataSnapshot child : dataSnapshot.getChildren()){
+            foods.add(child.getValue().toString());
         }
 
-        else{
-            dbRef.child("food").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for(DataSnapshot child : dataSnapshot.getChildren()){
-                        System.out.println(child);
-                        if(child.getValue().equals(food)){
-                            dbRef.child("food").child(child.getKey()).removeValue();
+        foodAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        foodLV.setAdapter(foodAdapter);
+
+        AdapterView.OnItemClickListener foodItemClick = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                TextView tv = (TextView) v;
+                final String food = tv.getText().toString();
+                dbRef.child("food").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot child : dataSnapshot.getChildren()){
+                            if(child.getValue().equals(food)){
+                                dbRef.child("food").child(child.getKey()).removeValue();
+                            }
                         }
                     }
-                }
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    //Do nothing
-                }
-            });
-        }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //Do nothing
+                    }
+                });
+                foodAdapter.remove(food);
+            }
+        };
 
-        removeFoodET.setText("");
+        foodLV.setOnItemClickListener(foodItemClick);
 
     }
 
